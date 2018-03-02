5c979a308758ec5fa998c3078dfd9ee959567da1
aleccunningham <aleccunningham96@gmail.com>
2018-03-01 18:41:55 -0600

    Sales Report dashboard

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,14 @@
 class WelcomeController < ApplicationController
   def index
+    def search
+      @results=0
+      if !params[:searchinput].nil?
+      @results=1
+      @searchinput = params[:searchinput]
+      @searchcriteria="%#{params[:searchinput]}%"
+      @courselist = Course.where("description like ?",@searchcriteria)
+      end
+    end
   end
 
   def help
